03430fd8dbe8835ed7a263d40f3b377833d73337
athulsukumaran <athulsukumaran@gmail.com>
2023-06-30 08:41:04 +0530

    initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,257 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    typescript,
+    html,
+    css,
+    reactjs,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    figma,
+    docker,
+    meta,
+    starbucks,
+    tesla,
+    shopify,
+    carrent,
+    ey,
+    optum,
+    infosys,
+    jobit,
+    tripguide,
+    threejs,
+  } from "../assets";
+  
+  export const navLinks = [
+    {
+      id: "about",
+      title: "About",
+    },
+    {
+      id: "work",
+      title: "Work",
+    },
+    {
+      id: "contact",
+      title: "Contact",
+    },
+  ];
+  
+  const services = [
+    {
+      title: "Web Developer",
+      icon: web,
+    },
+    {
+      title: "React Native Developer",
+      icon: mobile,
+    },
+    {
+      title: "Backend Developer",
+      icon: backend,
+    },
+    // {
+    //   title: "Content Creator",
+    //   icon: creator,
+    // },
+  ];
+  
+  const technologies = [
+    {
+      name: "HTML 5",
+      icon: html,
+    },
+    {
+      name: "CSS 3",
+      icon: css,
+    },
+    {
+      name: "JavaScript",
+      icon: javascript,
+    },
+    {
+      name: "TypeScript",
+      icon: typescript,
+    },
+    {
+      name: "React JS",
+      icon: reactjs,
+    },
+    // {
+    //   name: "Redux Toolkit",
+    //   icon: redux,
+    // },
+    {
+      name: "Tailwind CSS",
+      icon: tailwind,
+    },
+    {
+      name: "Node JS",
+      icon: nodejs,
+    },
+    {
+      name: "MongoDB",
+      icon: mongodb,
+    },
+    // {
+    //   name: "Three JS",
+    //   icon: threejs,
+    // },
+    {
+      name: "git",
+      icon: git,
+    },
+    // {
+    //   name: "figma",
+    //   icon: figma,
+    // },
+    {
+      name: "docker",
+      icon: docker,
+    },
+  ];
+  
+  const experiences = [
+    {
+      title: "Senior Systems Engineer",
+      company_name: "Infosys Ltd",
+      icon: infosys,
+      iconBg: "#383E56",
+      date: "Nov 2018 - Nov 2021",
+      points: [
+        "Utilized modern framework Angular to build dynamic and scalable applications.",
+        "Integrated RPA bots into applications using platforms like UiPath, AssistEdge to automate repetitive and rule-based tasks.",
+        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Participating in code reviews and providing constructive feedback to other developers.",
+      ],
+    },
+    {
+      title: "Software Engineer",
+      company_name: "Optum Global Solutions",
+      icon: optum,
+      iconBg: "#E6DEDD",
+      date: "Dec 2021 - Jan 2022",
+      points: [
+        "Developed and maintained scalable web applications, utilizing cutting-edge technologies and best practices to deliver robust and user-friendly solutions.",
+        "Implemented RESTful APIs using NodeJs and Express Framework for seamless integration with backend systems.",
+        "Designed and developed responsive web applications using modern front-end technologies like HTML, CSS, JavaScript, and AngularJS.",
+        "Collaborated with UI/UX designers and backend developers to implement responsive and intuitive user interfaces, optimizing performance and usability.",
+        "Conducted code reviews and implemented enhancements to improve code quality, maintainability, and overall system performance.",
+      ],
+    },
+    {
+      title: "Technical Lead",
+      company_name: "EY",
+      icon: ey,
+      iconBg: "#E6DEDD",
+      date: "Feb 2023 - Present",
+      points: [
+        "Led frontend development efforts for client projects, leveraging technologies like TypeScript and RxJs.",
+        "Worked closely with the design team to transform mockups into fully functional web applications.",
+        "Created dynamic and interactive user interfaces for enterprise-level web applications, employing modern frontend frameworks and libraries",
+        "Implemented best practices for code organization, performance optimization, and cross-browser compatibility to deliver high-quality frontend solutions.",
+      ],
+    },
+  ];
+  
+  const testimonials = [
+    {
+      testimonial:
+        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+      name: "Sara Lee",
+      designation: "CFO",
+      company: "Acme Co",
+      image: "https://randomuser.me/api/portraits/women/4.jpg",
+    },
+    {
+      testimonial:
+        "I've never met a web developer who truly cares about their clients' success like Rick does.",
+      name: "Chris Brown",
+      designation: "COO",
+      company: "DEF Corp",
+      image: "https://randomuser.me/api/portraits/men/5.jpg",
+    },
+    {
+      testimonial:
+        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+      name: "Lisa Wang",
+      designation: "CTO",
+      company: "456 Enterprises",
+      image: "https://randomuser.me/api/portraits/women/6.jpg",
+    },
+  ];
+  
+  const projects = [
+    {
+      name: "Car Rent",
+      description:
+        "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "mongodb",
+          color: "green-text-gradient",
+        },
+        {
+          name: "tailwind",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: carrent,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Job IT",
+      description:
+        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+      tags: [
+        {
+          name: "react",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "restapi",
+          color: "green-text-gradient",
+        },
+        {
+          name: "scss",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: jobit,
+      source_code_link: "https://github.com/",
+    },
+    {
+      name: "Trip Guide",
+      description:
+        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+      tags: [
+        {
+          name: "nextjs",
+          color: "blue-text-gradient",
+        },
+        {
+          name: "supabase",
+          color: "green-text-gradient",
+        },
+        {
+          name: "css",
+          color: "pink-text-gradient",
+        },
+      ],
+      image: tripguide,
+      source_code_link: "https://github.com/",
+    },
+  ];
+  
+  export { services, technologies, experiences, testimonials, projects };
